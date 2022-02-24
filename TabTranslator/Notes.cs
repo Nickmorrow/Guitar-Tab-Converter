647483e60fea71a6996e60ad774f347aad380e2f
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2022-02-23 22:24:04 -0500

    Add project files.

---
+++ TabTranslator/Notes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabTranslator
+{
+    internal class Notes
+    {
+        List<string> notes = new List<string>() { "C", "Csharp/Dflat", "D", "Dsharp/Eflat", "E", "F", "Fsharp/Gflat", "G", "Gsharp/Aflat", "A", "Asharp/Bflat", "B" };
+    }
+}
