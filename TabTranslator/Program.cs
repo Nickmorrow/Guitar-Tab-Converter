c6805a7534ed2a63c59aba25d4196ca1536e016b
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2022-03-09 20:54:28 -0500

    Trying out different methods to parse json and txt files.

--- TabTranslator/Program.cs
+++ TabTranslator/Program.cs
@@ -10,7 +10,17 @@ namespace TabTranslator
     {
         static void Main(string[] args)
         {
-            
+            var path = "E:/Projects/Programming/CSharp/RaketeMentoring/Final project/TabTranslator/SongsterrFiles/SongJSON/AliceInChainsWouldJSON.txt";
+            string json = System.IO.File.ReadAllText(path);
+            Welcome2 AliceInChains = JsonConvert.DeserializeObject<Welcome2>(json);
+             
+            Console.WriteLine(AliceInChains.Instrument.ToString());
+
+            var textPath = "E:/Projects/Programming/CSharp/RaketeMentoring/Final project/TabTranslator/SongsterrFiles/TextTabs/JeffBuckleyHallelujahTab.txt";
+            string text = System.IO.File.ReadAllText(textPath);
+            Console.WriteLine(text);
+
+
 
         }   
     }
