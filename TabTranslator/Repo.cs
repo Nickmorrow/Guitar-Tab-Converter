ee826034bd418de0cd9bad9efa16590e9cecfc55
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2022-08-03 10:42:03 -0400

    added class

---
+++ TabTranslator/Repo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace TabTranslator
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+    }
+}
+
