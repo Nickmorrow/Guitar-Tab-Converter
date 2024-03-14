a5582480800534946ff2e34ef30c64454a541f81
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2024-03-13 21:56:50 -0400

    Added a print button and download pdf button on the tab component.

---
+++ GuitarTabBlazorSite/wwwroot/DownloadPDF.js
@@ -0,0 +1,11 @@
+﻿window.downloadPDF = function (elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        var pdf = new jsPDF();
+        pdf.text(10, 10, 'PDF Title');
+        pdf.fromHTML(element, 10, 20);
+        pdf.save('tab_component.pdf');
+    } else {
+        console.error("Element with id '" + elementId + "' not found.");
+    }
+};
---
+++ GuitarTabBlazorSite/wwwroot/Print.js
@@ -0,0 +1,16 @@
+﻿
+window.printContent = function (elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        var printWindow = window.open('', '_blank');
+        printWindow.document.open();
+        printWindow.document.write('<html><head><title>Print</title><link rel="stylesheet" type="text/css" href="tabcomponentPrint.css"></head><body onload="window.print(); window.close();">');
+
+        printWindow.document.write(element.innerHTML);
+
+        printWindow.document.write('</body></html>');
+        printWindow.document.close();
+    } else {
+        console.error("Element with id '" + elementId + "' not found.");
+    }
+};
