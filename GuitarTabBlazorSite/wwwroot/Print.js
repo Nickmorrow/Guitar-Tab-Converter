221264d00192bda22d3369b4441e9c56e1e7d1b5
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2024-03-21 21:17:04 -0400

    Applied styling to tab component to make it more visually appealing as well as to fix issue with the div moving around the screen based on the song.

--- GuitarTabBlazorSite/wwwroot/Print.js
+++ GuitarTabBlazorSite/wwwroot/Print.js
@@ -4,10 +4,8 @@ window.printContent = function (elementId) {
     if (element) {
         var printWindow = window.open('', '_blank');
         printWindow.document.open();
-        printWindow.document.write('<html><head><title>Print</title><link rel="stylesheet" type="text/css" href="tabcomponentPrint.css"></head><body onload="window.print();">');
-
+        printWindow.document.write('<html><head><title>Print</title><link rel="stylesheet" type="text/css" href="tabcomponentPrint"></head><body onload="window.print();">');
         printWindow.document.write(element.innerHTML);
-
         printWindow.document.write('</body></html>');
         printWindow.document.close();
     } else {
