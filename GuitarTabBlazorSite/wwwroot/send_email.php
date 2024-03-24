a88f29459fd5f74ad31dfb819447d9cee061f901
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2024-03-24 19:00:43 -0400

    Linked new businuiss email to contact form and created php file to send emails.

---
+++ GuitarTabBlazorSite/wwwroot/send_email.php
@@ -0,0 +1,16 @@
+﻿<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "nmorrow@guitartabconverter.com"; // Replace with your email address
+    $subject = "Form Submission";
+    $message = "Name: " . $_POST["name"] . "\n";
+    $message .= "Email: " . $_POST["email"] . "\n";
+    $message .= "Message: " . $_POST["message"];
+    $headers = "From: " . $_POST["email"];
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error: Unable to send email.";
+    }
+}
+?>
