e8da63ec147b716c71c37bf547d382fc8bfb33aa
Nick Morrow <92399445+Nickmorrow@users.noreply.github.com>
2024-03-27 16:01:42 -0400

    typo in contact email

--- GuitarTabBlazorSite/wwwroot/send_email.php
+++ GuitarTabBlazorSite/wwwroot/send_email.php
@@ -1,6 +1,6 @@
 ﻿<?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "Contact@guitartabconverter.me"; // Replace with your email address
+    $to = "contact@guitartabconverter.me"; // Replace with your email address
     $subject = "Form Submission";
     $message = "Name: " . $_POST["name"] . "\n";
     $message .= "Email: " . $_POST["email"] . "\n";
